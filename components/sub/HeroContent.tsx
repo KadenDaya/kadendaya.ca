82d7cd39734fb9ca48883131fae5da0b72baa426
Kaden Daya <kaden.daya@icloud.com>
2025-10-29 17:04:16 -0400

    ..

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,7 +24,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            13 Year Old Developer Portfolio
           </h1>
         </motion.div>
 
@@ -45,8 +45,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          I like to build things.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
